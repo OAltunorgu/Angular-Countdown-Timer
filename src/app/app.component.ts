77b98fd330ce35043ad08d818810555482e8fd43
TCOALTUNORGU <ogulcan.altunorgu@kktcell.com>
2022-09-27 01:36:53 +0300

    Countdown Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'secimSayaci';
+  currentDate: any;
+  targetDate: any;
+  difference: any;
+  seconds: any;
+  minutes: any;
+  hours: any;
+  days: any;
+
+  x = setInterval(() => {
+    this.currentDate = new Date().getTime();
+    this.targetDate = new Date('June 18, 2023 07:00:00').getTime();
+    this.difference = this.targetDate - this.currentDate;
+    this.days = Math.floor(this.difference / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor(
+      (this.difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    this.minutes = Math.floor(
+      (this.difference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    this.seconds = Math.floor((this.difference % (1000 * 60)) / 1000);
+    if (this.difference < 0) {
+      clearInterval(this.x);
+      this.days = "Süre Doldu!";
+    }
+  }, 1000);
 }
---
+++ src/assets/js/activate.js
@@ -0,0 +1,12 @@
+$("#bg-canvas").bezierCanvas({
+    maxStyles: 1,
+    maxLines: 100,
+    lineSpacing: 1,
+    colorBase: {r: 255,g: 255,b: 255},
+    colorVariation: {r: 0, g: 0, b: 0},
+    moveCenterX: 0,
+    moveCenterY: 0,
+    speedVariation: 0.3,
+    globalAlpha: 1,
+    globalSpeed: 300,
+});	
